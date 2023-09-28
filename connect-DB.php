80814261808433648b21d5c4e5dca82e692db898
nick-altland <97930610+nick-altland@users.noreply.github.com>
2023-09-28 14:42:32 -0400

    Update connect-DB.php

--- connect-DB.php
+++ connect-DB.php
@@ -2,8 +2,8 @@
 <?php
 $databaseName = 'NALTLAND_labs';
 $dsn = 'mysql:host=webdb.uvm.edu;dbname=' . $databaseName;
-$username = 'naltland_writer';
-$password = 'GpPrWBg5FwFE';
+$username = '';
+$password = '';
 
 $pdo = new PDO($dsn, $username, $password);
 ?>
